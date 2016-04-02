1f509a7aa0a89d4e9f167fc02ac40c11b6c31511
LittleBird <ilms49898723@gmail.com>
2016-04-02 22:20:46 +0900

    finish all features, not well-tested

--- InstEncoder.cpp
+++ InstEncoder.cpp
@@ -9,23 +9,6 @@
 
 namespace lb {
 
-struct InstEncoder::InstDataEncode {
-    std::string opCode;
-    std::string rs, rt, rd;
-    std::string c;
-    std::string funct;
-    bool isValid;
-    InstDataEncode() {
-        opCode = "";
-        rs = "";
-        rt = "";
-        rd = "";
-        c = "";
-        funct = "";
-        isValid = false;
-    }
-};
-
 InstEncoder::InstEncoder() {
     init();
 }
@@ -35,49 +18,55 @@ InstEncoder::~InstEncoder() {
 
 void InstEncoder::init() {
     labelTable.clear();
-    pc = line = 0;
-    alive = false;
+    pc = preprocessPc = 0u;
+    line = 1;
 }
 
-unsigned InstEncoder::encodeInst(const std::string& inst) {
-    std::string val;
-    if (inst.back() == '\n') {
-        val = inst.substr(0, inst.length() - 1);
-    }
-    else {
-        val = inst;
-    }
-    unsigned data = analyzeString(val);
-    if (!isComment(val)) {
-        // pc offset if this inst is valid, not comment
-        pc += 4;
-        // line offset
-        ++line;
-    }
-    return data;
+void InstEncoder::setPc(const unsigned &pc) {
+    this->pc = pc;
+    this->preprocessPc = pc;
 }
 
-unsigned InstEncoder::analyzeString(std::string inst) {
-    // if this line is a comment
-    if (isComment(inst)) {
-        alive = false;
-        return 0xFFFFFFFFu;
+void InstEncoder::preProcess(std::string &src) {
+    if (isComment(src)) {
+        return;
     }
-    // check whether this line contains Label like "ELSE: "
-    if (hasLabel(inst)) {
-        for (unsigned i = 0; i < inst.length(); ++i) {
-            if (inst[i] == ':') {
-                std::string label = trimWhiteSpace(inst.substr(0, i + 1));
-                printf("label = \"%s\"\n", label.c_str());
-                labelTable.insert(make_pair(label, pc));
-                inst = inst.substr(i + 1);
+    if (hasLabel(src)) {
+        for (unsigned i = 0; i < src.length(); ++i) {
+            if (src[i] == ':') {
+                std::string label = trimWhiteSpace(src.substr(0, i));
+                src = src.substr(i + 1);
+                src = trimLeadingWhiteSpace(src);
+                labelTable.insert(make_pair(label, preprocessPc));
+                preprocessPc += 4;
+                return;
             }
         }
     }
+    else {
+        src = trimLeadingWhiteSpace(src);
+        preprocessPc += 4;
+        return;
+    }
+}
+
+InstEncodeData InstEncoder::encodeInst(const std::string& inst) {
+    std::string val = inst;
+    if (isComment(val)) {
+        ++line;
+        return InstEncodeData();
+    }
+    InstEncodeData data = analyzeString(val);
+    pc += 4;
+    ++line;
+    return data;
+}
+
+InstEncodeData InstEncoder::analyzeString(std::string inst) {
     inst = processInputString(inst);
     // nop
-    if (toLowerString(inst) == "nop") {
-        return 0u;
+    if (opToLower(inst) == "nop") {
+        return InstEncodeData(0u, true);
     }
     // normal case
     // for instruction operator
@@ -91,7 +80,7 @@ unsigned InstEncoder::analyzeString(std::string inst) {
             unsigned ret = 0u;
             ret |= funct & 0x3Fu;
             ret |= (rs & 0x1Fu) << 21;
-            return ret;
+            return InstEncodeData(ret, true);
         }
         else if (op == "sll" ||
                  op == "srl" ||
@@ -105,7 +94,7 @@ unsigned InstEncoder::analyzeString(std::string inst) {
             ret |= (rt & 0x1Fu) << 16;
             ret |= (rd & 0x1Fu) << 11;
             ret |= (c & 0x1Fu) << 6;
-            return ret;
+            return InstEncodeData(ret, true);
         }
         else {
             unsigned rs, rt, rd;
@@ -117,7 +106,7 @@ unsigned InstEncoder::analyzeString(std::string inst) {
             ret |= (rs & 0x1Fu) << 21;
             ret |= (rt & 0x1Fu) << 16;
             ret |= (rd & 0x1Fu) << 11;
-            return ret;
+            return InstEncodeData(ret, true);
         }
     }
     else if (instType == InstType::I) {
@@ -130,7 +119,7 @@ unsigned InstEncoder::analyzeString(std::string inst) {
             ret |= (opCode & 0x3Fu) << 26;
             ret |= (rt & 0x1Fu) << 16;
             ret |= (c & 0xFFFFu);
-            return ret;
+            return InstEncodeData(ret, true);
         }
         else if (op == "bgtz") {
             unsigned rs, c;
@@ -140,12 +129,39 @@ unsigned InstEncoder::analyzeString(std::string inst) {
             ret |= (opCode & 0x3Fu) << 26;
             ret |= (rs & 0x1Fu) << 21;
             ret |= (c & 0xFFFFu);
-            return ret;
+            return InstEncodeData(ret, true);
         }
         else if (op == "beq" || op == "bne") {
-            unsigned rs, c;
+            unsigned rs, rt, c;
             rs = getReg(nextString(inst));
+            rt = getReg(nextString(inst));
             c = getBranchC(nextString(inst));
+            unsigned ret = 0u;
+            ret |= (opCode & 0x3Fu) << 26;
+            ret |= (rs & 0x1Fu) << 21;
+            ret |= (rt & 0x1Fu) << 16;
+            ret |= (c & 0xFFFFu);
+            return InstEncodeData(ret, true);
+        }
+        else if (op == "lw" ||
+                 op == "lh" ||
+                 op == "lhu" ||
+                 op == "lb" ||
+                 op == "lbu" ||
+                 op == "sw" ||
+                 op == "sh" ||
+                 op == "sb") {
+            unsigned rt, rs;
+            unsigned c;
+            rt = getReg(nextString(inst));
+            c = getC(nextString(inst));
+            rs = getReg(nextString(inst));
+            unsigned ret = 0u;
+            ret |= (opCode & 0x3Fu) << 26;
+            ret |= (rs & 0x1Fu) << 21;
+            ret |= (rt & 0x1Fu) << 16;
+            ret |= (c & 0xFFFFu);
+            return InstEncodeData(ret, true);
         }
         else {
             unsigned rs, rt, c;
@@ -157,7 +173,7 @@ unsigned InstEncoder::analyzeString(std::string inst) {
             ret |= (rs & 0x1Fu) << 21;
             ret |= (rt & 0x1Fu) << 16;
             ret |= (c & 0xFFFFu);
-            return ret;
+            return InstEncodeData(ret, true);
         }
     }
     else if (instType == InstType::J) {
@@ -166,18 +182,56 @@ unsigned InstEncoder::analyzeString(std::string inst) {
         unsigned ret = 0u;
         ret |= (opCode & 0x3Fu) << 26;
         ret |= (c & 0x3FFFFFFu);
-        return ret;
+        return InstEncodeData(ret, true);
     }
     else if (instType == InstType::S) {
-        unsigned opCode = InstLookUp::translateToOpCode(op);
-        unsigned ret = 0u;
-        ret |= (opCode & 0x3Fu) << 26;
+        return InstEncodeData(0xFFFFFFFFu, true);
+    }
+    else {
+        fprintf(stderr, "Line %d: Unrecognized instruction %s\n", line, op);
+        return InstEncodeData(0xFFFFFFFFu, false);
+    }
+}
+
+unsigned InstEncoder::getReg(const std::string &src) {
+    std::string tmp = toLowerString(src);
+    if (tmp[0] == '$') {
+        tmp = tmp.substr(1);
+    }
+    unsigned regNum;
+    if (isdigit(tmp[0])) {
+        sscanf(tmp.c_str(), "%u", &regNum);
+    }
+    else {
+        regNum = InstLookUp::translateToReg(tmp);
+    }
+    return regNum;
+}
+
+unsigned InstEncoder::getC(const std::string &src) {
+    unsigned ret;
+    if (src.find("0x") != std::string::npos) {
+        sscanf(src.c_str(), "%x", &ret);
+    }
+    else {
+        sscanf(src.c_str(), "%d", &ret);
+    }
+    return ret;
+}
+
+unsigned InstEncoder::getBranchC(const std::string &src) {
+    unsigned ret;
+    if (sscanf(src.c_str(), "%x", &ret) == 1) {
         return ret;
     }
     else {
-        fprintf(stderr, "Line %d: Unrecognized instruction %s\n", line, instType);
-        alive = false;
-        return 0xFFFFFFFFu;
+        if (!labelTable.count(src)) {
+            fprintf(stderr, "Line %d: Undefined label: %s\n", line, src.c_str());
+            return 0u;
+        }
+        int targetAddr = labelTable[src];
+        int offset = (targetAddr - pc - 4) >> 2;
+        return toUnsigned(offset);
     }
 }
 
@@ -228,45 +282,6 @@ InstType InstEncoder::getInstType(const std::string& inst) {
     }
 }
 
-unsigned InstEncoder::getReg(const std::string &src) {
-    std::string tmp = toLowerString(src);
-    if (tmp[0] == '$') {
-        tmp = tmp.substr(1);
-    }
-    int regNum;
-    if (isdigit(tmp[0])) {
-        sscanf(tmp.c_str(), "%d", &regNum);
-    }
-    else {
-        regNum = InstLookUp::translateToReg(tmp);
-    }
-    return regNum;
-}
-
-unsigned InstEncoder::getC(const std::string &src) {
-    unsigned ret;
-    if (src.find("0x") != std::string::npos) {
-        std::string val = src.substr(2);
-        sscanf(val.c_str(), "%x", &ret);
-    }
-    else {
-        sscanf(src.c_str(), "%d", &ret);
-    }
-    return ret;
-}
-
-unsigned InstEncoder::getBranchC(const std::string &src) {
-    unsigned ret;
-    if (sscanf(src.c_str(), "%x", &ret) == 1) {
-        return ret;
-    }
-    else {
-        int targetAddr = labelTable[src];
-        int offset = pc - targetAddr;
-        return offset;
-    }
-}
-
 bool InstEncoder::hasLabel(const std::string& src) {
     return src.find(":", 0) != std::string::npos;
 }
@@ -282,71 +297,98 @@ bool InstEncoder::isComment(const std::string& src) {
     return false;
 }
 
-std::string InstEncoder::processInputString(const std::string &inst) {
-    std::string ret = "";
-    unsigned idx = 0;
-    for (idx = 0; idx < inst.length(); ++idx) {
-        if (inst[idx] != ' ') {
-            break;
-        }
-    }
-    for ( ; idx < inst.length(); ++idx) {
-        ret += inst[idx];
-        if (inst[idx] == ' ') {
-            break;
-        }
-    }
-    for ( ; idx < inst.length(); ++idx) {
-        if (inst[idx] == '$') {
-            ret += " ";
-        }
-        if (inst[idx] == ',') {
-            ret += " ";
-            continue;
-        }
-        if (inst[idx] == ' ') {
-            continue;
-        }
-        ret += inst[idx];
-    }
-    return ret;
-}
-
-std::string InstEncoder::toLowerString(const std::string& src) {
-    std::string ret = src;
-    for (unsigned i = 0; i < ret.length(); ++i) {
-        ret[i] = tolower(ret[i]);
-    }
-    return ret;
-}
-
 std::string InstEncoder::nextString(std::string &src) {
     char inputBuffer[2048];
     std::string stringBuffer;
     sscanf(src.c_str(), "%s", inputBuffer);
-    stringBuffer = toLowerString(inputBuffer);
-    unsigned leadingWhiteSpace = 0;
-    while (leadingWhiteSpace < src.length() && src[leadingWhiteSpace] == ' ') {
-        ++leadingWhiteSpace;
+    stringBuffer = inputBuffer;
+    unsigned deleteIdx = 0;
+    while (deleteIdx < src.length() && src[deleteIdx] == ' ') {
+        ++deleteIdx;
     }
-    if (leadingWhiteSpace >= src.length()) {
+    while (deleteIdx < src.length() && src[deleteIdx] != ' ') {
+        ++deleteIdx;
+    }
+    if (deleteIdx >= src.length()) {
         src = "";
     }
     else {
-        src = src.substr(leadingWhiteSpace + stringBuffer.length());
+        src = src.substr(deleteIdx);
     }
     return stringBuffer;
+}
+
+std::string InstEncoder::opToLower(const std::string &src) {
+    std::string ret = src;
+    for (unsigned i = 0; i < ret.length(); ++i) {
+        if (ret[i] == ' ') {
+            break;
+        }
+        ret[i] = static_cast<char>(tolower(ret[i]));
+    }
+    return ret;
+}
+
+std::string InstEncoder::toLowerString(const std::string &src) {
+    std::string ret = src;
+    for (unsigned i = 0; i < ret.length(); ++i) {
+        ret[i] = static_cast<char>(tolower(ret[i]));
+    }
+    return ret;
+}
+
+std::string InstEncoder::processInputString(const std::string &inst) {
+    std::string val = inst;
+    std::string ret = "";
+    val = trimLeadingWhiteSpace(val);
+    unsigned idx = 0;
+    for ( ; idx < val.length(); ++idx) {
+        ret += val[idx];
+        if (val[idx] == ' ') {
+            break;
+        }
+    }
+    for ( ; idx < val.length(); ++idx) {
+        if (val[idx] == ',') {
+            ret += " ";
+            continue;
+        }
+        if (val[idx] == '$') {
+            ret += " ";
+        }
+        if (val[idx] == ' ') {
+            continue;
+        }
+        ret += val[idx];
+    }
+    return ret;
 }
 
 std::string InstEncoder::trimWhiteSpace(const std::string &src) {
     std::string ret = "";
     for (unsigned i = 0; i < src.length(); ++i) {
-        if (src[i] == ' ' || src[i] == ':') {
+        if (src[i] == ' ') {
             continue;
+        }
+        if (src[i] == '#') {
+            break;
         }
         ret += src[i];
     }
     return ret;
+}
+
+std::string InstEncoder::trimLeadingWhiteSpace(const std::string &src) {
+    unsigned idx = 0;
+    while (idx < src.length() && src[idx] == ' ') {
+        ++idx;
+    }
+    if (idx >= src.length()) {
+        return "";
+    }
+    else {
+        return src.substr(idx);
+    }
 }
 
 } /* namespace lb */
